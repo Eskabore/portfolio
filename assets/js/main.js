0784953bb949141747457faf7da5af69c3f3d1c5
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-02-17 13:29:06 +0100

    Contacts section + CSS + Form&Email.js/%20config

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,72 +1,126 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 /*=============== END JS PORTFOLIO ===============*/
 /*=============== START JS WATCHES ===============*/
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
-    const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                       : header.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SWIPER PROJECTS ===============*/
 let swiperProjects = new Swiper(".projects__container", {
-    spaceBetween: 24,
-    loop: 'true',
+  spaceBetween: 24,
+  loop: "true",
 
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: -56,
     },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-    breakpoints: {
-        1200: {
-          slidesPerView: 2,
-          spaceBetween: -56,
-        },
-    },
+  },
 });
 
-/*=============== SWIPER PROJECTS ===============*/
+/*=============== SWIPER TESTIMONIALS ===============*/
 var swiperTestimonial = new Swiper(".testimonial__container", {
-    grabCursor: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactName = document.getElementById("contact-name"),
+  contactEmail = document.getElementById("contact-email"),
+  contactProject = document.getElementById("contact-project"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // Check if field is not empty
+  if (
+    contactName.value.trim() === "" ||
+    contactEmail.value.trim() === "" ||
+    contactProject.value.trim() === ""
+  ) {
+    // Add and remove color
+    contactMessage.classList.remove("color-blue");
+    contactMessage.classList.add("color-red");
+
+    contactMessage.textContent = "Please fill in all fields 📩";
+  } else {
+    // service ID - template ID - #form - public key
+    emailjs
+      .sendForm(
+        "service_c6yubog",
+        "template_w7obp7h",
+        "#contact-form",
+        "3_buLaZ5yOpc0WlRh"
+      )
+      .then(
+        () => {
+          // Show success message and add color
+          contactMessage.classList.add("color-blue");
+          contactMessage.textContent = "Message sent successfully ✅";
+
+          // Clear form after 5 seconds
+          setTimeout(() => {
+            contactName.value = "";
+            contactEmail.value = "";
+            contactProject.value = "";
+            contactMessage.textContent = "";
+          }, 5000);
+        },
+        (error) => {
+          alert("Error: " + error.message);
+        }
+      );
+  }
+};
+
+contactForm.addEventListener("submit", sendEmail);
